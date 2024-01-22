d10a22106ecab70c21a8749acbf8b03b136c6095
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-22 13:01:11 -0500

    Update and rename geometry_latest_release.py to geometry_category_statistics.py

---
+++ geometry_category_statistics.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from read_data import read_from_gsheets
+import altair as alt
+from datetime import datetime, timedelta
+import pandas as pd
+import streamlit.components.v1 as components
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Geometry Summary Statistics - Latest Release",
+    layout="wide"
+)
+
+#### Geometry Category Stats ####
+
+
+category_stats_df = read_from_gsheets("Category stats")\
+    [["Country", "naics_2", "naics_code", "safegraph_category", "safegraph_subcategory", "industry_title", "total_poi_count", "poi_with_polygon_count", "Polygon coverage"]]\
+    .astype({'total_poi_count': int,"poi_with_polygon_count":int, "Polygon coverage":float })
+
+global_df = category_stats_df.groupby(['naics_2', 'industry_title'])\
+    .agg(total_poi_count=('total_poi_count', 'sum'), poi_with_polygon_count=("poi_with_polygon_count", "sum"))\
+    .sort_values('poi_with_polygon_count', ascending=False)\
+    .reset_index()\
+    .rename(columns={"naics_2": "2-digit NAICS", "industry_title": "Industry Title", "total_poi_count": "Total POI", "poi_with_polygon_count":"POI with polygon count"})
+global_df['Overall Polygon Coverage'] = (global_df['POI with polygon count'] /global_df['Total POI']) * 100
+
+global_df_styled = global_df.drop(["Total POI"], axis=1).style.apply(lambda x: ['background-color: #D7E8ED' if i % 2 == 0 else '' for i in range(len(x))], axis=0)\
+    .format({"POI with polygon count": "{:,}",
+              "Overall Polygon Coverage": "{:.01f}%"})
+
+countries = ['US', 'UK', 'CA']
+dfs = []
+
+for country in countries:
+    df = (
+        category_stats_df[category_stats_df['Country'] == country]
+        [["naics_code", "safegraph_category", "safegraph_subcategory", "poi_with_polygon_count", "Polygon coverage"]]
+        .rename(columns={"naics_code": "NAICS Code", "safegraph_category": "SafeGraph Category",\
+                         "safegraph_subcategory": "SafeGraph Subcategory", "poi_with_polygon_count": "POI with Polygon Count"})
+        .assign(**{
+            "Polygon coverage": lambda df: ((df["Polygon coverage"]) * 100).astype(float)
+    }).sort_values('POI with Polygon Count', ascending=False)
+        .reset_index(drop=True)
+    )
+
+    df['POI with Polygon Count'] = df['POI with Polygon Count'].astype(int).apply(lambda x: "{:,}".format(x))
+    df['Polygon coverage'] = df['Polygon coverage'].astype(float).apply(lambda x: "{:.01f}%".format(x))
+    dfs.append(df)
+
+styled_dfs = [
+    df.style.apply(lambda x: ['background-color: #D7E8ED' if i % 2 == 0 else '' for i in range(len(x))], axis=0)
+    for df in dfs
+]
+
+tabs = st.tabs(["Global"] + countries)
+with tabs[0]:
+    # st.write("Global POI Count")
+    st.dataframe(global_df_styled, use_container_width=True, hide_index=True)
+
+for i, tab in enumerate(tabs[1:]):
+    with tab:
+        if i < len(styled_dfs):
+            # st.write(f"{countries[i]} POI Count")
+            st.dataframe(styled_dfs[i], use_container_width=True,hide_index=True)
